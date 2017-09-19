b8155e59226f0760db1c17a375ed928801885e52
Marwan Zogheib <mzogheib@gmail.com>
2017-09-19 19:58:32 +1000

    LECTURE-39 [reducer_books] add a books reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,13 @@
+export default function () {
+    return [
+        {
+            title: 'Book 1'
+        },
+        {
+            title: 'Book 2'
+        },
+        {
+            title: 'Book 3'
+        }
+    ];
+}

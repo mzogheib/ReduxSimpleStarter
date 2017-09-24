22db6d052d4e6cf87b411658500ec70f59054fd9
Marwan Zogheib <mzogheib@gmail.com>
2017-09-24 12:36:24 +1000

    LECTURE-49 [book_detail] display selected book detail or instruction if none selected

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,13 +1,16 @@
 export default function () {
     return [
         {
-            title: 'Book 1'
+            title: 'Book 1',
+            pages: 101
         },
         {
-            title: 'Book 2'
+            title: 'Book 2',
+            pages: 80
         },
         {
-            title: 'Book 3'
+            title: 'Book 3',
+            pages: 234
         }
     ];
 }

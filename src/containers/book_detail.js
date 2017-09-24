15ec2e888f372d7bd820010f068130a7c0a818a3
Marwan Zogheib <mzogheib@gmail.com>
2017-09-24 11:50:27 +1000

    LECTURE-48 [book_detail] promote to a container

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookDetail extends Component {
+class BookDetail extends Component {
     render () {
         return (
             <div>Book Detail!</div>
         );
     }
 }
+
+function mapStateToProps (state) {
+    // whatever gets returned from here will show up as props in BookDetail
+    return {
+        book: state.activeBook
+    };
+}
+
+// Promote BookDetail from a component to a container - it needs to know
+// about this new dispatch method, selectBook. Make it available as a prop
+export default connect(mapStateToProps)(BookDetail);

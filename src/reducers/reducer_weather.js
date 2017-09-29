6b8d9ea29da393739b92a8f10bef23b7e33a3722
Marwan Zogheib <mzogheib@gmail.com>
2017-09-29 16:42:32 +1000

    LECTURE-60 [reducer_weather] return the fetched weather data

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,10 @@
-export default function (state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function (state = [], action) {
+    switch (action.type) {
+        case FETCH_WEATHER:
+            // Insert new record to the start of the state array
+            return [action.payload.data, ...state];
+    }
     return state;
 }
